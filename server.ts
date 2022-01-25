7c6887175759026782c86e19443b866b4ff81517
Jamie Lou <jlou711@googlemail.com>
2022-01-25 14:08:14 +0000

    Add endpoints to get songs and add new song

--- server.ts
+++ server.ts
@@ -11,8 +11,8 @@ config(); //Read .env file lines as though they were env vars.
 //For the ssl property of the DB connection config, use a value of...
 // false - when connecting to a local DB
 // { rejectUnauthorized: false } - when connecting to a heroku DB
-const herokuSSLSetting = { rejectUnauthorized: false }
-const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
+const herokuSSLSetting = { rejectUnauthorized: false };
+const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
@@ -21,21 +21,89 @@ const dbConfig = {
 const app = express();
 
 app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
+app.use(cors()); //add CORS support to each following route handler
 
 const client = new Client(dbConfig);
 client.connect();
 
-app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+app.get("/songs", async (req, res) => {
+  const dbres = await client.query("SELECT * FROM songs");
+  const response = [];
+
+  for (const song of dbres.rows) {
+    const artistGenre: { [key: string]: Array<string> } = {};
+    const dbresArtist = await client.query(
+      "SELECT * FROM artists WHERE song_uri = $1",
+      [song.uri]
+    );
+    // Get artists associated with song
+    for (const artist of dbresArtist.rows) {
+      const dbresGenre = await client.query(
+        "SELECT genre FROM genres WHERE artist_uri = $1",
+        [artist.artist_uri]
+      );
+      // Append genres to associated artist
+      artistGenre[artist.artist_name] = [
+        ...dbresGenre.rows.map((genre) => genre.genre),
+      ];
+    }
+    response.push({
+      ...song,
+      artists: artistGenre,
+    });
+  }
+  res.status(200).json(response);
 });
 
+// When adding a song, also add any new artists and their genres
+app.post("/songs", async (req, res) => {
+  const { uri, name, album, album_artwork, release_date, artists } = req.body;
+  const checkURI = await client.query("SELECT * FROM songs where uri = $1", [
+    uri,
+  ]);
+  let dbresSong;
+  try {
+    if (checkURI.rowCount === 0) {
+      dbresSong = await client.query(
+        "INSERT INTO songs VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [uri, name, album, album_artwork, release_date]
+      );
+      for (let artist of artists) {
+        await client.query(
+          "INSERT INTO artists (artist_uri,song_uri,artist_name) VALUES ($1, $2, $3)\
+       ON CONFLICT (artist_uri, song_uri) DO NOTHING",
+          [artist.artist_uri, uri, artist.artist_name]
+        );
+        const checkArtistGenre = await client.query(
+          "SELECT * FROM genres where artist_uri = $1",
+          [artist.artist_uri]
+        );
+        if (checkArtistGenre.rowCount === 0) {
+          for (let genre of artist.genres)
+            await client.query(
+              "INSERT INTO genres (artist_uri,genre) VALUES ($1, $2)",
+              [artist.artist_uri, genre]
+            );
+        }
+      }
+      res.status(201).json({ message: "Success", data: dbresSong.rows });
+    } else {
+      res
+        .status(400)
+        .json({ message: "Error - duplicate song URI found", data: {} });
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Internal server error trying to create add new song",
+      data: {},
+    });
+  }
+});
 
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
-  throw 'Missing PORT environment variable.  Set it in .env file.';
+  throw "Missing PORT environment variable.  Set it in .env file.";
 }
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
--- setup.sql
+++ setup.sql
@@ -4,38 +4,51 @@ CREATE TABLE songs (
   album VARCHAR(255) NOT NULL,
   album_artwork VARCHAR(255),
   release_date VARCHAR(255),
-  popularity INT
-)
+  likes INT,
+  dislikes INT
+);
 
 CREATE TABLE artists (
-  artist_uri VARCHAR(255) PRIMARY KEY,
+  artist_uri VARCHAR(255) NOT NULL,
   song_uri VARCHAR(255) NOT NULL,
   artist_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (artist_uri, song_uri),
   FOREIGN KEY (song_uri) REFERENCES songs(uri)
-)
+);
 
 CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
     artist_uri VARCHAR(255) NOT NULL,
-    genre VARCHAR(255) NOT NULL,
-    FOREIGN KEY (artist_uri) REFERENCES artists(artist_uri)
-)
+    genre VARCHAR(255) NOT NULL
+);
 
 INSERT INTO songs VALUES 
   ('02MWAaffLxlfxAUY7c5dvx', 'Heat Waves', 'Dreamland (+ Bonus Levels)','https://i.scdn.co/image/ab67616d00001e02eeef81a914a1db21799c8322', '2020-08-06'),
   ('04QTmCTsaVjcGaoxj8rSjE', 'Know Your Worth', 'Know Your Worth','https://i.scdn.co/image/ab67616d00001e026d8139ae2807fb26071e75d9', '2020-04-23'),
-  ('0ERnYArznxdTBEIj1VSta8', 'man i is', 'No Pressure','https://i.scdn.co/image/ab67616d00001e021c76e29153f29cc1e1b2b434', '2020-07-24')
+  ('0ERnYArznxdTBEIj1VSta8', 'man i is', 'No Pressure','https://i.scdn.co/image/ab67616d00001e021c76e29153f29cc1e1b2b434', '2020-07-24');
 
   INSERT INTO artists VALUES 
   ('4yvcSjfu4PC0CYQyLy4wSq','02MWAaffLxlfxAUY7c5dvx','Glass Animals'),
   ('6LuN9FCkKOj5PcnpouEgny','04QTmCTsaVjcGaoxj8rSjE','Khalid'),
   ('6nS5roXSAGhTGr34W6n7Et','04QTmCTsaVjcGaoxj8rSjE','Disclosure'),
   ('0Y3agQaa6g2r0YmHPOO9rh','04QTmCTsaVjcGaoxj8rSjE','DaVido'),
-  ('4xRYI6VqpkE3UwrDrAZL8L','0ERnYArznxdTBEIj1VSta8','Logic')
+  ('4xRYI6VqpkE3UwrDrAZL8L','0ERnYArznxdTBEIj1VSta8','Logic');
 
   INSERT INTO genres VALUES
-  (DEFAULT, '4yvcSjfu4PC0CYQyLy4wSq', 'gauze pop;indietronica;shiver pop'),
-  (DEFAULT, '6LuN9FCkKOj5PcnpouEgny', 'pop;pop r&b'),
-  (DEFAULT, '6nS5roXSAGhTGr34W6n7Et', 'house;pop;tropical house;uk dance'),
-  (DEFAULT, '0Y3agQaa6g2r0YmHPOO9rh', 'afropop;ghanaian hip hop;nigerian pop'),
-  (DEFAULT, '4xRYI6VqpkE3UwrDrAZL8L', 'conscious hip hop;dmv rap;hip hop;pop rap;rap')
+  (DEFAULT, '4yvcSjfu4PC0CYQyLy4wSq', 'gauze pop'),
+  (DEFAULT, '4yvcSjfu4PC0CYQyLy4wSq', 'indietronica'),
+  (DEFAULT, '4yvcSjfu4PC0CYQyLy4wSq', 'shiver pop'),
+  (DEFAULT, '6LuN9FCkKOj5PcnpouEgny', 'pop'),
+  (DEFAULT, '6LuN9FCkKOj5PcnpouEgny', 'pop r&b'),
+  (DEFAULT, '6nS5roXSAGhTGr34W6n7Et', 'house'),
+  (DEFAULT, '6nS5roXSAGhTGr34W6n7Et', 'pop'),
+  (DEFAULT, '6nS5roXSAGhTGr34W6n7Et', 'tropical house'),
+  (DEFAULT, '6nS5roXSAGhTGr34W6n7Et', 'uk dance'),
+  (DEFAULT, '0Y3agQaa6g2r0YmHPOO9rh', 'afropop'),
+  (DEFAULT, '0Y3agQaa6g2r0YmHPOO9rh', 'ghanaian hip hop'),
+  (DEFAULT, '0Y3agQaa6g2r0YmHPOO9rh', 'nigerian pop'),
+  (DEFAULT, '4xRYI6VqpkE3UwrDrAZL8L', 'conscious hip hop'),
+  (DEFAULT, '4xRYI6VqpkE3UwrDrAZL8L', 'dmv rap'),
+  (DEFAULT, '4xRYI6VqpkE3UwrDrAZL8L', 'hip hop'),
+  (DEFAULT, '4xRYI6VqpkE3UwrDrAZL8L', 'pop rap'),
+  (DEFAULT, '4xRYI6VqpkE3UwrDrAZL8L', 'rap');
